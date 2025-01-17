3136f44fad4cbc529d3ba4b93c5e3cd2139aef66
Hugh Davenport <hugh@davenport.net.nz>
2025-01-17 17:24:18 +1300

    Move git init to it's own function

--- src/main.c
+++ src/main.c
@@ -4,6 +4,28 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+int init_command() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    fprintf(stderr, "Logs from your program will appear here!\n");
+
+    if (mkdir(".git", 0755) == -1 ||
+        mkdir(".git/objects", 0755) == -1 ||
+        mkdir(".git/refs", 0755) == -1) {
+        fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
+        return 1;
+    }
+
+    FILE *headFile = fopen(".git/HEAD", "w");
+    if (headFile == NULL) {
+        fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
+        return 1;
+    }
+    fprintf(headFile, "ref: refs/heads/main\n");
+    fclose(headFile);
+
+    printf("Initialized git directory\n");
+}
+
 int main(int argc, char *argv[]) {
     // Disable output buffering
     setbuf(stdout, NULL);
@@ -17,25 +39,7 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
 
     if (strcmp(command, "init") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
-
-        if (mkdir(".git", 0755) == -1 ||
-            mkdir(".git/objects", 0755) == -1 ||
-            mkdir(".git/refs", 0755) == -1) {
-            fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
-            return 1;
-        }
-
-        FILE *headFile = fopen(".git/HEAD", "w");
-        if (headFile == NULL) {
-            fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
-            return 1;
-        }
-        fprintf(headFile, "ref: refs/heads/main\n");
-        fclose(headFile);
-
-        printf("Initialized git directory\n");
+        return init_command();
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
         return 1;
